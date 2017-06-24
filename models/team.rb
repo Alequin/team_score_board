a89ee59bde2b77d119dbdf772440598c78cf3a0e
James Cox <j-cox03@hotmail.com>
2017-06-24 16:53:18 +0100

    Created file for Team class and written starts of the init method

---
+++ models/team.rb
@@ -0,0 +1,19 @@
+require_relative("../database/databaseassistant.rb")
+
+class Team
+
+  def initialize(options)
+    options = fill_empty_attributes(options)
+    @name = options["name"]
+    @score = options["score"].to_i
+  end
+
+  private
+
+  def fill_empty_attributes(options)
+     options["name"] = "No team name" if(options["name"])
+     options["score"] = 0 if(options["score"])
+  end
+
+
+end
