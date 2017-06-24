f179f93c97571d4661d2312b4ae4a84c151c447b
James Cox <j-cox03@hotmail.com>
2017-06-24 23:31:47 +0100

    Applied basics for control panel. Some layout issues

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require("sinatra")
 require("sinatra/contrib/all")
 
 require_relative("controllers/team_controller.rb")
+require_relative("controllers/control_panel_controller.rb")
 
 get("/") do
   redirect to("/team")
--- controllers/control_panel_controller.rb
+++ controllers/control_panel_controller.rb
@@ -4,5 +4,10 @@ require("sinatra/contrib/all")
 require_relative("../models/team.rb")
 
 get("/panel") do
+  @teams = Team.get_all("id", "ASC")
+  erb(:"control_panel/index")
+end
+
+post("/panel/:id") do
   
 end
