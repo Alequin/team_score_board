ecedf3989cfc08a2da85dfc121571cb9b0462a0e
James Cox <j-cox03@hotmail.com>
2017-06-24 17:24:16 +0100

    Seeds test saving data to table using the Team class (works)

--- database/seeds/seeds.rb
+++ database/seeds/seeds.rb
@@ -1,11 +1,17 @@
 require_relative("../../models/team.rb")
 
-team_1 = Team.new({
+teams = []
+
+teams[0] = Team.new({
   "name" => "Winners",
   "score" => 9
 })
 
-team_2 = Team.new({
+teams[1] = Team.new({
   "name" => "better Winners",
   "score" => 10
 })
+
+teams.each() do |team|
+  team.save()
+end
