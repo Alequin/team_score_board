864d60d82270092e58759c23bcc0d6129dc6c1a3
James Cox <j-cox03@hotmail.com>
2017-06-24 21:34:44 +0100

    Added code which should sort and update teams in team controller

--- controllers/team_controller.rb
+++ controllers/team_controller.rb
@@ -5,7 +5,7 @@ require_relative("../other/presenter_manager.rb")
 require_relative("../models/team.rb")
 
 get("/team") do
-  @teams = Team.get_all()
+  @teams = Team.get_all("id", "ASC")
   @teams = PresenterManager.split_teams_into_rows(@teams)
   erb(:"presenter/index")
 end
@@ -14,7 +14,10 @@ post("/team/presenter/:id") do
   team = Team.find_by_id(params["id"])
   amount = PresenterManager.increment_value
   form_value = params["edit_score"]
+
   team.increase_score(amount) if(form_value == "+")
   team.decrease_score(amount) if(form_value == "-")
+
+  team.update
   redirect to("/team")
 end
