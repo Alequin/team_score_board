38e0d9931c9a5f49ed0b0288ad5fed9a50f0943a
James Cox <j-cox03@hotmail.com>
2017-06-24 21:09:36 +0100

    Made more test teams in seeds to view in browser

--- database/seeds/seeds.rb
+++ database/seeds/seeds.rb
@@ -13,6 +13,26 @@ teams[0] = Team.new({
 
 teams[1] = Team.new({
   "name" => "better Winners",
+  "score" => 10000
+})
+
+teams[2] = Team.new({
+  "name" => "We are awesome",
+  "score" => -100
+})
+
+teams[3] = Team.new({
+  "name" => "The team of winners who are good at winning and not loosing",
+  "score" => 999
+})
+
+teams[4] = Team.new({
+  "name" => "Frogs",
+  "score" => 5
+})
+
+teams[5] = Team.new({
+  "name" => "Onion Eaters",
   "score" => 10
 })
 
