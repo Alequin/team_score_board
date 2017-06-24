fb8c93f71f1fc66be3998218fc25072ec1f9f440
James Cox <j-cox03@hotmail.com>
2017-06-24 17:18:55 +0100

    Made seed files to team model

---
+++ database/seeds/seeds.rb
@@ -0,0 +1,11 @@
+require_relative("../../models/team.rb")
+
+team_1 = Team.new({
+  "name" => "Winners",
+  "score" => 9
+})
+
+team_2 = Team.new({
+  "name" => "better Winners",
+  "score" => 10
+})
