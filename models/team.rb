0aff67af9c89052678fdfb2bde421572791b47e3
James Cox <j-cox03@hotmail.com>
2017-06-24 17:23:31 +0100

    Made save and update methods in class Team

--- models/team.rb
+++ models/team.rb
@@ -12,12 +12,27 @@ class Team < DatabaseAssistant
     @score = options["score"].to_i
   end
 
+  def save()
+    super(get_table_hash)
+  end
+
+  def update()
+    super(get_table_hash)
+  end
+
   private
 
   def fill_empty_attributes(options)
      options["name"] = "No team name" if(Validator.input_empty?(options["name"]))
      options["score"] = 0 if(Validator.input_empty?(options["score"]))
      return options
+  end
+
+  def get_table_hash()
+    table = {
+      "name" => @name,
+      "score" => @score
+    }
   end
 
 end
