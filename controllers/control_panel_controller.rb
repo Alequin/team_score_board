0d0f3d9926e4190830a0ed0a9dc965a1a32f27ea
James Cox <j-cox03@hotmail.com>
2017-06-25 20:14:22 +0100

    extended control_panel_controller to use more specific routes and moved views into controller_panel file into more specific folders

--- controllers/control_panel_controller.rb
+++ controllers/control_panel_controller.rb
@@ -3,25 +3,29 @@ require("sinatra/contrib/all")
 
 require_relative("../models/team.rb")
 
-get("/panel") do
+get("/panel/team") do
   @teams = Team.get_all("id", "ASC")
-  erb(:"control_panel/index")
+  erb(:"control_panel/teams/index")
 end
 
-post("/panel") do
+get("/panel/settings") do
+
+end
+
+post("/panel/team") do
   team = Team.new(params)
   team.save()
-  redirect to("/panel")
+  redirect to("/panel/team")
 end
 
-post("/panel/:id") do
+post("/panel/team/:id") do
   team = Team.new(params)
   team.update
-  redirect to("/panel")
+  redirect to("/panel/team")
 end
 
-post("/panel/:id/delete") do
+post("/panel/team/:id/delete") do
   team = Team.find_by_id(params["id"])
   team.delete if(params["delete_check"])
-  redirect to("/panel")
+  redirect to("/panel/team")
 end
